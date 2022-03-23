6285d0669cc231d7d8bdd65fe09bbf8d46e3b020
Kirill Ki <kirillkalapov@gmail.com>
2022-03-23 23:40:40 +0100

    Initial Commit

---
+++ QuickNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuickNews
+//
+//  Created by  Mr.Ki on 23.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
